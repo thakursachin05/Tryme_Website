139814ec78ea7452b79202804018dd002fdf4c75
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-04-01 10:09:02 +0530

    Changes in project code, new files has been added

---
+++ controllers/orders.controller.js
@@ -0,0 +1,72 @@
+const stripe = require('stripe')('sk_test_51KOxVjSG64QDRRDDl5Fj3og8154LWdtxErStdZuEn3UbwFU7bMuMrLR9JRyTAPNxdAOJRryxyneegVfzQOdtXH2V00xJ548uRf')
+const Order = require('../models/order.model');
+const User = require('../models/user.model');
+
+async function getOrders(req, res) {
+  try {
+    const orders = await Order.findAllForUser(res.locals.uid);
+    res.render('customer/orders/all-orders', {
+      orders: orders,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function addOrder(req, res, next) {
+  const cart = res.locals.cart;
+
+  let userDocument;
+  try {
+    userDocument = await User.findById(res.locals.uid);
+  } catch (error) {
+    return next(error);
+  }
+
+  const order = new Order(cart, userDocument);
+
+  try {
+    await order.save();
+  } catch (error) {
+    next(error);
+    return;
+  }
+
+  req.session.cart = null;
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: cart.items.map(function(item) {
+      return  {
+        price_data: {
+          currency: 'inr',
+          product_data: {
+            name: item.product.title
+          },
+          unit_amount: +item.product.price.toFixed(2) * 100
+        },
+        quantity: item.quantity,
+      }
+    }),
+    mode: 'payment',
+    success_url: `http://localhost:3000/orders/success`,
+    cancel_url: `http://localhost:3000/orders/failure`,
+  });
+
+  res.redirect(303, session.url);
+}
+
+function getSuccess(req, res) {
+  res.render('customer/orders/success');
+}
+
+function getFailure(req, res) {
+  res.render('customer/orders/failure');
+}
+
+module.exports = {
+  addOrder: addOrder,
+  getOrders: getOrders,
+  getSuccess: getSuccess,
+  getFailure: getFailure
+};
